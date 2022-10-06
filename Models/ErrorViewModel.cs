52bf426aac161f283480ed60702c5c4c84034bbc
Danilo Santos <artededan@protonmail.ch>
2022-10-05 22:15:55 -0300

    (UTI Processual) Initial ASP.NET Core web application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UTIProcessual.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
