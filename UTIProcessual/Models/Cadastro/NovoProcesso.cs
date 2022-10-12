6a103108a89f9f52b4094f1b7039408735abde3b
Danilo Santos <artededan@protonmail.ch>
2022-10-12 13:38:46 -0300

    (UTI Processual) Models: Cadastro: Adicione classe para adição de processo

---
+++ UTIProcessual/Models/Cadastro/NovoProcesso.cs
@@ -0,0 +1,32 @@
+﻿namespace UTIProcessual.Models.Cadastro
+{
+    public class NovoProcesso
+    {
+        // Processo
+
+        //OBS: Id = NumeroProcesso
+        public Guid Id { get; set; }
+        public int NumeroProcessoPenal { get; set; }
+        public string TipoPenal { get; set; }
+
+        // Dados do Crime
+        public string LocalDoCrime { get; set; }
+        public DateTime DataDoCrime { get; set; }
+        public int NumeroPCNET { get; set; }
+        public int NumeroREDS { get; set; }
+        public int NumInqueritoPolicial { get; set; }
+
+        // Réu
+        public string NomeReu { get; set; }
+        public string FiliacaoReu { get; set; }
+        public DateTime DataNascReu { get; set; }
+        public string NaturalidadeReu { get; set; }
+        public string EnderecoReu { get; set; }
+        public int TelefoneReu { get; set; }
+
+        // Caso o réu tenha patrocinio
+        public Boolean Advogado { get; set; }
+        public string OABAdvogado { get; set; }
+        public string DefensoriaPublica { get; set; }
+    }
+}
