959125fbf14ec69d3774dc5affadfa237c5ce137
Danilo Santos <artededan@protonmail.ch>
2022-10-22 23:21:53 -0300

    (UTI Processual) MVC: Crie a página de visualização / edição de processos

--- UTIProcessual/Controllers/ProcessosController.cs
+++ UTIProcessual/Controllers/ProcessosController.cs
@@ -59,5 +59,78 @@ namespace UTIProcessual.Controllers
             await dbContext.SaveChangesAsync();
             return RedirectToAction("ListarProcessos");
         }
+
+
+        [HttpGet]
+        public async Task <IActionResult> View(Guid id)
+        {
+            var verProcesso = await dbContext.CadastroProcessos.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (verProcesso != null)
+            {
+                var viewModel = new EditarProcessoViewModel()
+                {
+                    Id = verProcesso.Id,
+                    NumeroProcessoPenal = verProcesso.NumeroProcessoPenal,
+                    TipoPenal = verProcesso.TipoPenal,
+                    LocalDoCrime = verProcesso.LocalDoCrime,
+                    DataDoCrime = verProcesso.DataDoCrime,
+                    NumeroPCNET = verProcesso.NumeroPCNET,
+                    NumeroREDS = verProcesso.NumeroREDS,
+                    NumInqueritoPolicial = verProcesso.NumInqueritoPolicial,
+
+                    NomeReu = verProcesso.NomeReu,
+                    FiliacaoReu = verProcesso.FiliacaoReu,
+                    DataNascReu = verProcesso.DataNascReu,
+                    NaturalidadeReu = verProcesso.NaturalidadeReu,
+                    EnderecoReu = verProcesso.EnderecoReu,
+                    TelefoneReu = verProcesso.TelefoneReu,
+
+                    Advogado = verProcesso.Advogado,
+                    OABAdvogado = verProcesso.OABAdvogado,
+                    DefensoriaPublica = verProcesso.DefensoriaPublica
+                };
+
+                return await Task.Run(() => View("View", viewModel));
+            }
+
+            return RedirectToAction("View");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(EditarProcessoViewModel model)
+        {
+            var verProcesso = await dbContext.CadastroProcessos.FindAsync(model.Id);
+
+            if (verProcesso != null)
+            {
+                verProcesso.Id = model.Id;
+                verProcesso.NumeroProcessoPenal = model.NumeroProcessoPenal;
+                verProcesso.TipoPenal = model.TipoPenal;
+                verProcesso.LocalDoCrime = model.LocalDoCrime;
+                verProcesso.DataDoCrime = model.DataDoCrime;
+                verProcesso.NumeroPCNET = model.NumeroPCNET;
+                verProcesso.NumeroREDS = model.NumeroREDS;
+                verProcesso.NumInqueritoPolicial = model.NumInqueritoPolicial;
+
+                verProcesso.NomeReu = model.NomeReu;
+                verProcesso.FiliacaoReu = model.FiliacaoReu;
+                verProcesso.DataNascReu = model.DataNascReu;
+                verProcesso.NaturalidadeReu = model.NaturalidadeReu;
+                verProcesso.EnderecoReu = model.EnderecoReu;
+                verProcesso.TelefoneReu = model.TelefoneReu;
+
+                verProcesso.Advogado = model.Advogado;
+                verProcesso.OABAdvogado = model.OABAdvogado;
+                verProcesso.DefensoriaPublica = model.DefensoriaPublica;
+
+                await dbContext.SaveChangesAsync();
+
+                return RedirectToAction("ListarProcessos");
+            }
+
+            return RedirectToAction("ListarProcessos");
+        }
+
     }
 }
