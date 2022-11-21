80b5eca7ac8e24f721f9361b14806d144328d68c
Jessepuc <90655284+Jessepuc@users.noreply.github.com>
2022-11-21 09:55:28 -0300

    Dashboard changes

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using utip_backend.Models;
+using utip_backend.Data;
 namespace utip_backend.Controllers
 {
     public class DashboardController : Controller
     {
         private readonly ILogger<DashboardController> _logger;
+
+        private ProcessDbContext db = new ProcessDbContext();
 
         public DashboardController(ILogger<DashboardController> logger)
         {
@@ -14,8 +17,8 @@ namespace utip_backend.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
+            return View(db.Processo.ToList());     
+         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
