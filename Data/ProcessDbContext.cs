ec6672268035a04fcfba5835b13ecabb89f675ab
Jessepuc <90655284+Jessepuc@users.noreply.github.com>
2022-12-06 22:21:42 -0300

    Set CreatedDate Na criação do processo

--- Data/ProcessDbContext.cs
+++ Data/ProcessDbContext.cs
@@ -3,10 +3,22 @@ using utip_backend.Models;
 
 namespace utip_backend.Data
 {
-    public class ProcessDbContext:DbContext
+    public class ProcessDbContext : DbContext
     {
-        public ProcessDbContext(DbContextOptions<ProcessDbContext> options) : base(options) 
-        {            
+        public ProcessDbContext(DbContextOptions<ProcessDbContext> options) : base(options)
+        {
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var AddedEntities = ChangeTracker.Entries().Where(E => E.State == EntityState.Added).ToList();
+
+            AddedEntities.ForEach(E =>
+            {
+                E.Property("CreatedDate").CurrentValue = DateTime.Now;
+            });
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
         }
 
         public DbSet<ProcessModel> Processo { get; set; }
